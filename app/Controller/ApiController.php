3b871e97f2ca37f7a0d6b48a600b26d739cf358a
Karina Obermeier <karina@infinixsoft.com>
2017-03-01 11:22:29 -0300

    add api SUBSCRIPTIONS

--- app/Controller/ApiController.php
+++ app/Controller/ApiController.php
@@ -1,10 +1,18 @@
 <?php
+App::import('Controller', 'Facebook');
+App::uses('CakeText', 'Utility');
+App::uses('Security', 'Utility');
+App::uses('BlowfishPasswordHasher', 'Controller/Component/Auth');
 class ApiController extends AppController {
+	public $helpers = array('Text');
 	public $components = array("RequestHandler");
 
 	public function beforeFilter() {
     	parent::beforeFilter();
     	$this->autoRender = false;
+
+    	header("Content-Type: application/json");
+        $this->Auth->allow('subscriptions');
 	}
 	
 	public function sucursales() {
@@ -13,5 +21,34 @@ class ApiController extends AppController {
 		$stores = $this->Store->find('all',array('order'=>array('Store.name ASC')));
 		return json_encode($stores);
 	}
+
+
+	//get , http://development.infinixsoft.com/chatelet-web/api/subscriptions
+    public function subscriptions(){
+          
+    $this->loadModel('Subscriptions'); 
+    $result = array();
+    if (!empty($this->request->is('get'))) {
+        $Subscriptions = $this->Subscriptions->find('all',array('order'=>array('Subscriptions.id DESC')));
+  
+        if(!empty($Subscriptions)){  
+
+         foreach($Subscriptions as &$item){
+            
+          
+               
+                $result[] = $item['Subscriptions']; 
+            
+         }
+          header('200, SUBSCRIPTIONS_UNSUCCESSFUL');
+          return(json_encode($result));   
+         }else {
+
+         header('409, SUBSCRIPTIONS_UNSUCCESSFUL');
+          die(json_encode(array("detail"=>"Subscriptions queried unsuccessfully",
+                                "code"=>"SUBSCRIPTIONS_UNSUCCESSFUL")));  
+        }
+     }
+    }
 }
 ?>
