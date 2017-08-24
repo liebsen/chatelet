4bc21986315745a074b7ab907f6a29e54817623e
rodolfo <rodolfo@infinixsoft.com>
2017-08-24 16:42:04 -0300

    admin login now is redirect to cms

--- app/Controller/AppController.php
+++ app/Controller/AppController.php
@@ -87,6 +87,10 @@ class AppController extends Controller
         if(!empty($this->Auth->user('role')) && $this->Auth->user('role') == 'admin'){
             $site_visits = $this->site_visits();
             $this->set('site_visits',$site_visits);
+            if ($this->request->params['controller']!='admin') {
+                $this->redirect('/admin');
+            }
+            
         }
 
         $this->loadModel('Setting');
