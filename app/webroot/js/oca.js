b4106aa7e50e569be0b9735989833159727c361a
Francisco Marasco <fmarasco@kubikware.com>
2019-10-04 16:08:58 -0300

    Remove delivery cost before nov

--- app/webroot/js/oca.js
+++ app/webroot/js/oca.js
@@ -77,7 +77,7 @@ $(function(){
 			onErrorAlert('No tienes productos en el carrito.');
 			return false;
 		}
-		if( !a || !b || !c || (1>parseFloat($('#cost').text()) && (subtotal<3200 || !isDateBeforeToday(new Date(2019, 10, 1)) ))) {
+		if( !a || !b || !c || (1>parseFloat($('#cost').text()) && subtotal<3200 && isDateBeforeToday(new Date(2019, 10, 1)) )) {
 			$('#cp').focus();
 			$('#cp').removeClass('ok');
 			$('#cp').addClass('wrong');
