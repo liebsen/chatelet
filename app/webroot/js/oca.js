374c2c9e6357eb3fa1c962a9effeab98153dc0af
Francisco Marasco <fmarasco@kubikware.com>
2019-10-04 15:50:19 -0300

    Remove delivery cost before nov

--- app/webroot/js/oca.js
+++ app/webroot/js/oca.js
@@ -47,7 +47,7 @@ $(function(){
 				if (!json.price || parseInt(json.price) == 0){
 					json.price = 114;
 				}
-        if (subtotal >= 3200 || !isDateBeforeToday(new Date(2019, 11, 1))) {
+        if (subtotal >= 3200 || !isDateBeforeToday(new Date(2019, 10, 1))) {
           console.log('Envio gratis');
           $('#cost').text( 0 );
 					$('#free_delivery').text('Envio gratis!');
