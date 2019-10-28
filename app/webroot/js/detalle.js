98810a0cac71c6dd88b377f24a63a59efbac2367
Francisco Marasco <fmarasco@kubikware.com>
2019-10-28 12:47:19 -0300

    Resize images

--- app/webroot/js/detalle.js
+++ app/webroot/js/detalle.js
@@ -24,7 +24,7 @@ function showDivs(n) {
   }
   x[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " w3-opacity-off";
- 
+
 }
 //$(".mySlides").elevateZoom();
     CloudZoom.quickStart();
@@ -37,7 +37,7 @@ $(function(){
     for(var i=0;i<images.length;i++){
       if(images[i]!=""){
         moreviews += '<li class="dontResize"><a href="#"><img  class="demo w3-opacity w3-hover-opacity-off img-responsive" onclick="currentDiv('+(i+ 1)+')"  id="img_01" src="https://d3baxuoyqsgua.cloudfront.net/thumb_'+images[i]+'" ></a></li>';
-        surround += '<img  class="mySlides cloudzoom img-responsive"  id="mySlides zoom1"   style="" src="https://d3baxuoyqsgua.cloudfront.net/'+images[i]+'" data-cloudzoom=\'zoomSizeMode:"image",autoInside: 600\'/>';
+        surround += '<img  class="mySlides cloudzoom img-responsive"  id="mySlides zoom1"   style="" src="https://d3baxuoyqsgua.cloudfront.net/'+images[i]+'" data-cloudzoom=\'zoomSizeMode:"zoom",autoInside: 200\'/>';
       }
     }
     $("#ul-moreviews").html(moreviews);
