a461f0e6a12aebcb7d8e518d49e24f4735db336e
liebsen <overlemonsoft@gmail.com>
2021-02-10 21:21:22 -0300

    export dump mysqli

--- app/webroot/export_dump.php
+++ app/webroot/export_dump.php
@@ -10,35 +10,42 @@ $csv_filename = 'export_products.csv';
 // database variables
 $hostname = "localhost";
 $user = "root";
-$password = "Chat3leta$.";
+//$password = "n@XBQVPgb4b4BddJ3D3dns4Xg!";
+$password = "coala090";
 $database = "chatelet";
 
+function mysqli_field_name($result, $field_offset)
+{
+    $properties = mysqli_fetch_field_direct($result, $field_offset);
+    return is_object($properties) ? $properties->name : null;
+}
+
 // Database connecten voor alle services
-mysql_connect($hostname, $user, $password)
-or die('Could not connect: ' . mysql_error());
+$link = mysqli_connect($hostname, $user, $password, $database)
+or die('Could not connect: ' . mysqli_error($link));
 					
-mysql_select_db($database)
-or die ('Could not select database ' . mysql_error());
+// mysqli_select_db($database)
+// or die ('Could not select database ' . mysqli_error());
 
 // create var to be filled with export data
 $csv_export = '';
 
 // query to get data from database
-$query = mysql_query("SELECT * FROM ".$db_record." ".$where);
-$field = mysql_num_fields($query);
+$query = mysqli_query($link, "SELECT * FROM ".$db_record." ".$where);
+$field = mysqli_num_fields($query);
 
 // create line with field names
 for($i = 0; $i < $field; $i++) {
-  $csv_export.= mysql_field_name($query,$i).',';
+  $csv_export.= mysqli_field_name($query,$i).',';
 }
 // newline (seems to work both on Linux & Windows servers)
 $csv_export.= '
 ';
 
-while($row = mysql_fetch_array($query)) {
+while($row = mysqli_fetch_array($query)) {
   // create line with field values
   for($i = 0; $i < $field; $i++) {
-    $csv_export.= '"'.$row[mysql_field_name($query,$i)].'",';
+    $csv_export.= '"'.$row[mysqli_field_name($query,$i)].'",';
   }	
   $csv_export.= '
 ';	
