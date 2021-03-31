36f23dacb2444cf2a5201251fda9c6c4486666e5
liebsen <overlemonsoft@gmail.com>
2021-03-31 15:37:23 -0300

    build

--- app/Controller/AdminController.php
+++ app/Controller/AdminController.php
@@ -206,7 +206,7 @@ class AdminController extends AppController {
 					$data['message'] = 'Ya enviado previamente';
 				}
 				// $data['url'] = "https://www1.oca.com.ar/ocaepak/Envios/EtiquetasCliente.asp?IdOrdenRetiro={$sale['def_orden_retiro']}&CUIT=30-71119953-1";
-				$data['url'] = "http://www5.oca.com.ar/OcaEPakNet/Views/Impresiones/Etiquetas.aspx?IdOrdenRetiro={$sale['def_orden_retiro']}&CUIT=30-71119953-1"
+				$data['url'] = "http://www5.oca.com.ar/OcaEPakNet/Views/Impresiones/Etiquetas.aspx?IdOrdenRetiro={$sale['def_orden_retiro']}&CUIT=30-71119953-1";
 			}
 		}
 		die(json_encode($data));
