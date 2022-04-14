0e2a3c38382218bb9c5adf410bec3cd0708a38f5
liebsen <overlemonsoft@gmail.com>
2022-04-14 09:01:23 -0300

    build

--- app/Controller/CarritoController.php
+++ app/Controller/CarritoController.php
@@ -428,7 +428,7 @@ class CarritoController extends AppController
 		$user['telephone'] = @preg_replace("/[^0-9]/","",$user['telephone']);
 		$user['floor'] = (!empty($user['floor']))?$user['floor']:'';
 		$user['depto'] = (!empty($user['depto']))?$user['depto']:'';
-		$user['coupon'] = (!empty($user['coupon']))?strtoupper($user['depto']):'';
+		$user['coupon'] = (!empty($user['coupon']))?strtoupper($user['coupon']):'';
 		$user['regalo'] = (isset($user['regalo']) && $user['regalo']==='on'?1:0);
 		if(!$this->request->is('post') || $user['cargo'] === 'shipment' && empty($user['postal_address']) || empty($user['street_n']) || empty($user['street']) || empty($user['localidad']) || empty($user['provincia']) || empty($user['name']) || empty($user['surname']) || empty($user['email']) || empty($user['telephone'])){
 			$this->Session->setFlash(
@@ -629,7 +629,8 @@ class CarritoController extends AppController
 		);
 		error_log(json_encode($to_save));
 		$this->Sale->save($to_save);
-
+		die();
+		
 		//MP
 		$mp = new MP(Configure::read('client_id'), Configure::read('client_secret'));
 		$success_url = Router::url(array('controller' => 'carrito', 'action' => 'clear'), true);
