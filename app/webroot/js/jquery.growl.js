8083bc211d9dbb0422eb97c92bf0e4116eba0592
liebsen <overlemonsoft@gmail.com>
2022-06-01 20:18:14 -0300

    build

--- app/webroot/js/jquery.growl.js
+++ app/webroot/js/jquery.growl.js
@@ -42,7 +42,7 @@ Copyright 2015 Kevin Sylvestre
   Growl = (function() {
     Growl.settings = {
       namespace: 'growl',
-      duration: 10000,
+      duration: 15000,
       close: "&#215;",
       location: "default",
       style: "default",
