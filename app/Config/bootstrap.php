b4063231f69c3cfa6dc1f89a725a862cc489d689
liebsen <overlemonsoft@gmail.com>
2022-08-25 21:31:04 -0300

    build

--- app/Config/bootstrap.php
+++ app/Config/bootstrap.php
@@ -25,7 +25,7 @@
 // Setup a 'default' cache configuration for use in the application.
 Cache::config('default', array('engine' => 'File'));
 Configure::write('mUrl','');
-Configure::write('APP_DIST', '2.82');
+Configure::write('APP_DIST', '2.83');
 
 /**
  * The settings below can be used to set additional paths to models, views and controllers.
