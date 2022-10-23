a14abeea527ab9dcf49d27bcf25606d0cf22d914
liebsen <overlemonsoft@gmail.com>
2022-10-23 13:14:38 -0300

    build

--- commit.php
+++ commit.php
@@ -4,7 +4,7 @@ require_once __DIR__ . '/app/functions.php';
 
 $path = __DIR__ .'/app/version';
 $version = (float) file_get_contents($path);
-$version+= 0.1;
+$version+= 0.001;
 var_dump($version);
 log2file($path, $version, 'w');
 
