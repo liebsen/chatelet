ba8212823f18a94b789a669603380c64c41ec16b
Martin <overlemonsoft@gmail.com>
2025-09-05 11:37:55 -0300

    fix: header clunky behavior --env stage

--- app/Controller/Component/SQLComponent.php
+++ app/Controller/Component/SQLComponent.php
@@ -7,12 +7,13 @@ class SQLComponent extends Component {
 		//$myServer = "181.164.35.14";
 		$myServer = "181.98.11.110";
 
-		//$myUser = "sa";
-		$myUser = "UsuarioWebChatelet";
-		//$myPass = "infinixchatelet2011";
+		/*$myUser = "UsuarioWebChatelet";
 		$myPass = "UsuWEBChatelet1973!!";
-		//$myDB = "Ventas";
-		$myDB = "WebChatelet";
+		$myDB = "WebChatelet";*/
+
+		$myUser = "sa";
+		$myPass = "infinixchatelet2011";
+		$myDB = "Ventas";
 
 		try {
 			$this->conn = new PDO("dblib:host=$myServer;dbname=$myDB", $myUser, $myPass);
