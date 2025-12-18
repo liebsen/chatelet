2a38754a96ec3989690afa41207b873f1acce672
Martín Frith <telemagico@gmail.com>
2025-12-18 13:24:37 -0300

    automerge

--- app/webroot/js/envio.js
+++ app/webroot/js/envio.js
@@ -252,6 +252,11 @@ $(document).ready(function() {
 	$(document).on('click', 'a[href="#envio"]', function(e){
 		const cp = $('.input-cp')
 		const validateForm = $("#envio_form").bootstrapValidator('validate');
+		const validForm = $("#envio_form").data('bootstrapValidator').isValid();
+		if(!validForm) {
+			$('input[type="submit"]').prop('disabled', true);
+		}
+		
 		if(!cp.val() || cp.val() == '') {
 			if(localStorage.lastcp) {
 				cp.val(localStorage.lastcp)	
