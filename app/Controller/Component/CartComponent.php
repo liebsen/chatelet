ef88526de56e5c2b759a0e6030172aac067c20b5
Martín Frith <telemagico@gmail.com>
2025-12-18 18:40:07 -0300

    automerge

--- app/Controller/Component/CartComponent.php
+++ app/Controller/Component/CartComponent.php
@@ -277,11 +277,11 @@ class CartComponent extends Component {
 		$shipping_price_min = $settings['shipping_price_min'];
     $bank_free_shipping = $settings['bank_free_shipping'];
 
-    CakeLog::write('debug', 'shipping_type:'.json_encode($shipping_type));
-    CakeLog::write('debug', 'shipping_zips:'.json_encode($shipping_zips));
-    CakeLog::write('debug', 'shipping_price_min:'.json_encode($shipping_price_min));
-    CakeLog::write('debug', 'bank_free_shipping:'.json_encode($bank_free_shipping));
-    CakeLog::write('debug', 'payment_method(2):'.json_encode($payment_method));
+    // CakeLog::write('debug', 'shipping_type:'.json_encode($shipping_type));
+    // CakeLog::write('debug', 'shipping_zips:'.json_encode($shipping_zips));
+    // CakeLog::write('debug', 'shipping_price_min:'.json_encode($shipping_price_min));
+    // CakeLog::write('debug', 'bank_free_shipping:'.json_encode($bank_free_shipping));
+    // CakeLog::write('debug', 'payment_method(2):'.json_encode($payment_method));
 
     if(!empty($bank_free_shipping) && $payment_method == 'bank') {
       return true;
@@ -305,7 +305,7 @@ class CartComponent extends Component {
 			}
 			// error_log('shipping_value: '.@$shipping_config['Setting']['value']);
 		}
-    CakeLog::write('debug', 'free_shipping(2):'.json_encode($free_shipping));
+    // CakeLog::write('debug', 'free_shipping(2):'.json_encode($free_shipping));
 
 		return $free_shipping;
 		// return intval($price) >= intval($shipping_price['Setting']['value']);
