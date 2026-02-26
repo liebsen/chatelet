08a28ed264499389d2ee617e17f03f9ed509a3c6
Martín Frith <telemagico@gmail.com>
2026-02-26 11:53:59 -0300

    fix: Sucursales phones

--- app/View/Helper/AppHelper.php
+++ app/View/Helper/AppHelper.php
@@ -247,7 +247,7 @@ class AppHelper extends Helper {
       if(!empty($old_price) && abs($old_price-$price > 1)) {
         $str.= '<span class="old_price text-muted">'.\price_format($old_price) . '</span>';
       }
-      $str.= '<span class="text-muted">' . (strlen($text) ? "{$text}" : "") . '</span>';
+      $str.= '<span class="text-sm">' . (strlen($text) ? "{$text}" : "") . '</span>';
       $str.= '</div>';
     }
     
@@ -279,7 +279,7 @@ class AppHelper extends Helper {
     $str2 = '';
     $str2.='<div class="legends-container"><span class="legends w-100">';
     if($bank_price && $text != 'Transferencia') {
-      $str2.= "<div class='price-list justify-content-start'><span class='price_strong'>" .\price_format($bank_price)." </span><span class='text-theme text-bold product-badge'>-".@$discount."%</span> <span class='text-muted'>Transferencia</span> </div>";
+      $str2.= "<div class='price-list justify-content-start'><span class='price_strong'>" .\price_format($bank_price)." </span><span class='text-theme text-bold product-badge'>-".@$discount."%</span> <span class='text-sm'>Transferencia</span> </div>";
     }
     if($mp_price && $text != 'Mercado Pago'){
       $str2.= "<div class='price-list justify-content-start'><span class='price_strong'>" .\price_format($mp_price)." </span> <span class='text-theme text-bold product-badge'>-".@$discount."%</span> <span class='text-sm'>Mercado Pago</span> </div>";
