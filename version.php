ea781b45f69e13697efd0ffaac8430a890ae79ec
Martín Frith <telemagico@gmail.com>
2026-02-28 12:03:52 -0300

    feat: css refactor

--- version.php
+++ version.php
@@ -4,8 +4,7 @@ require_once __DIR__ . '/app/functions.php';
 $path = __DIR__ .'/app/version';
 
 $version = (int) file_get_contents($path);
-var_dump($version);
 $version+= 1;
-var_dump($version);
+
 log2file($path, $version, 'w');
 //var_dump('v'. ($version / 10000));
