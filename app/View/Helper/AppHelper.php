aa261a27993324ef4f50d1b87859bd050941f824
Martín Frith <telemagico@gmail.com>
2026-03-06 11:35:55 -0300

    fix: hide repeated prices

--- app/View/Helper/AppHelper.php
+++ app/View/Helper/AppHelper.php
@@ -238,8 +238,6 @@ class AppHelper extends Helper {
       }
     }
 
-
-
     // price
     if(!$noprice) {
       $str = '<div class="price-list justify-content-start">';
@@ -247,7 +245,9 @@ class AppHelper extends Helper {
       if(!empty($old_price) && abs($old_price-$price > 1)) {
         $str.= '<span class="old_price text-muted">'.\price_format($old_price) . '</span>';
       }
-      $str.= '<span class="text-muted">' . (strlen($text) ? "{$text}" : "") . '</span>';
+      if(abs($mp_price-$price > 1) || abs($bank_price-$price > 1)) {
+        $str.= '<span class="text-muted">' . (strlen($text) ? "{$text}" : "") . '</span>';
+      }
       $str.= '</div>';
     }
     
@@ -278,10 +278,10 @@ class AppHelper extends Helper {
 
     $str2 = '';
     $str2.='<div class="legends-container"><span class="legends w-100">';
-    if($bank_price && $text != 'Transferencia') {
+    if($bank_price && $text != 'Transferencia' && abs($bank_price-$price > 1)) {
       $str2.= "<div class='price-list justify-content-start'><span class='price_strong'>" .\price_format($bank_price)." </span><span class='text-theme text-bold product-badge'>-".@$discount."%</span> <span class='text-muted'>Transferencia</span> </div>";
     }
-    if($mp_price && $text != 'Mercado Pago'){
+    if($mp_price && $text != 'Mercado Pago' && abs($mp_price-$price > 1)){
       $str2.= "<div class='price-list justify-content-start'><span class='price_strong'>" .\price_format($mp_price)." </span> <span class='text-theme text-bold product-badge'>-".@$discount."%</span> <span class='text-sm'>Mercado Pago</span> </div>";
     }
 
