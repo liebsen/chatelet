5dd0dd24139bd639bc07f8745b53a3912493ccc3
Martín Frith <telemagico@gmail.com>
2026-03-20 16:56:31 -0300

    fix: Newsletter forms

--- app/webroot/js/webpush.js
+++ app/webroot/js/webpush.js
@@ -19,7 +19,7 @@ function urlBase64ToUint8Array(base64String) {
 }
 
 // Your public VAPID key generated from your server
-const publicVapidKey = 'BEBiooz0kvrLqazPF8zdDj9SC_It9_KiZ-0iOp16Ks93U6S-G45i7woIqFUmtZZYgh_tWVXfr88etWr0jKtFcyY'; //
+const publicVapidKey = 'BEBiooz0kvrLqazPF8zdDj9SC_It9_KiZ-0iOp16Ks93U6S-G45i7woIqFUmtZZYgh_tWVXfr88etWr0jKtFcyY';
 const version = 1001;
 
 async function subscribeUser() {
@@ -32,7 +32,8 @@ async function subscribeUser() {
   const registration = await navigator.serviceWorker.register(`/service-worker.js?v=${version}`); //
   console.log('Service Worker registered');
 
-  const permission = await Notification.requestPermission(); //
+  const permission = await Notification.requestPermission();
+  console.log('permission',permission)
   if (permission !== 'granted') {
     throw new Error('Notification permission not granted.');
   }
@@ -42,18 +43,25 @@ async function subscribeUser() {
     applicationServerKey: urlBase64ToUint8Array(publicVapidKey), //
   };
 
-  const subscription = await registration.pushManager.subscribe(subscribeOptions); //
-  console.log('User subscribed:', subscription);
+  let subscription = await registration.pushManager.getSubscription();
 
-  // Send the subscription to your server
-  await fetch('/api/subscribe', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(subscription),
-  });
-  console.log('Subscription sent to server');
+  if(subscription) {
+    // already subscribed on this device
+    console.log('User already subscribed:', subscription);
+  } else {
+    subscription = await registration.pushManager.subscribe(subscribeOptions);
+    console.log('User subscribed:', subscription);
+
+    // Send the subscription to your server
+    await fetch('/api/subscribe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(subscription),
+    });
+    console.log('Subscription sent to server');
+  }
 }
 
 // Call this function when appropriate (e.g., a button click, after user consent)
