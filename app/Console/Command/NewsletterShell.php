a7922f8f9461759ba782fff005f6c4c0e6a13d11
Martín Frith <telemagico@gmail.com>
2026-03-20 20:55:09 -0300

    fix: Newsletter forms

--- app/Console/Command/NewsletterShell.php
+++ app/Console/Command/NewsletterShell.php
@@ -149,9 +149,9 @@ class NewsletterShell extends AppShell {
   public function sendPush($data, $push) {
     var_dump("----sendPush");
     var_dump($push['Webpush']['payload']);
-    var_dump(json_decode($push['Webpush']['payload']));
+    var_dump(json_decode($push['Webpush']['payload'], true));
     $push = array(
-      'subscription' => Subscription::create( json_decode($push['Webpush']['payload']) ),
+      'subscription' => Subscription::create( json_decode($push['Webpush']['payload'], true) ),
         /*array(
           'endpoint' => 'https://fcm.googleapis.com/fcm/send/djRg_IDPtSs:APA91bFwYCC73F4X3cXELK...',
           'keys' => array(
