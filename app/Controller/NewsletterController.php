f113419e50f2274d6d89e4f2bd9dc78e5a226fd3
Martín Frith <telemagico@gmail.com>
2026-04-20 13:38:22 -0300

    feat: Newsletter group by author

--- app/Controller/NewsletterController.php
+++ app/Controller/NewsletterController.php
@@ -267,6 +267,10 @@ class NewsletterController extends AppController {
     }
     \d("show_social(1)",$this->settings['newsletter_show_social']);
     \d("show_social(2)",$newsletter['Newsletter']['show_social']);
+    \d("show_social(3)",(
+      	$this->settings['newsletter_show_social'] == '1' && 
+      	$newsletter['Newsletter']['show_social'] == '1'
+      ));
     $viewVars = array(
       'data' => $newsletter,
       'products' => $products,
