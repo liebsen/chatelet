da733c6d85544e1afef57d05fd23847db3a86eef
Martín Frith <telemagico@gmail.com>
2026-07-08 14:29:44 -0300

    autocommit

--- app/Controller/CheckoutController.php
+++ app/Controller/CheckoutController.php
@@ -166,7 +166,7 @@ class CheckoutController extends AppController
 				$cart_totals[$part] = $data[$part];
 			}
 
-			$cart_totals['postal_address'] = intval($cart_totals['postal_address']);
+			//$cart_totals['postal_address'] = intval($cart_totals['postal_address']);
 			// CakeLog::write('debug', 'envio(cart_totals)'.json_encode($cart_totals));
 			
 			$this->Cart->update(null, $cart_totals);
